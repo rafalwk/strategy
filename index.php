4490beb4b7f819506d3ec9e9ab2106c065a8bfa0
Rafał Wójcik <rafalwooojcik@gmail.com>
2022-03-14 15:29:59 +0100

    added form

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-
-echo 'STRATEGY DESIGN PATTERN';
+<form action="FindCar.php">
+    <label>
+        <select name="body">
+            <option>Sedan</option>
+            <option>Van</option>
+        </select>
+        <select name="fuel">
+            <option>Diesel</option>
+            <option>Petrol</option>
+            <option>Electric</option>
+        </select>
+    </label>
+    <input type="submit" value="Send">
+</form>
